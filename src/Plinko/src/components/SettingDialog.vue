854dbbcda5f8f99054bdc31e07f8d92adf378239
rocky <rocky@evervictory-it.com>
2024-12-09 09:22:18 +0800

    add setting dialog

---
+++ src/Plinko/src/components/SettingDialog.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+const { t: $t  } = useI18n()
+import { useAppStore } from '@/stores/app'
+const appStore = useAppStore()
+
+
+</script>
+
+<template>
+  <div v-show='appStore.settingDialog.visible' @click="appStore.settingDialog.visible = false" class='modal-backdrop fixed w-full h-full left-0 right-0 top-0 bottom-0 z-20'></div>
+
+  <div v-show='appStore.settingDialog.visible' class='modal-container pt-3 pb-8 z-50 text-red-800'>
+    <div class='modal-content mx-auto text-left pt-1 mt-4 pb-6 flex flex-col gap-5' >
+      <button><img src="@/assets/images/ico_sound.svg" />{{$t('Sound')}}</button>
+      <button>{{$t('BetLimit')}}</button>
+      <button>{{$t('GameRule')}}</button>
+      <button>{{$t('Fairness')}}</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-container{
+  background: #262626;
+  border-radius: 6px;
+  width: 340px;
+  height: 290px;
+  max-height:580px;
+  margin:0 auto;
+  position: absolute;
+  color: #fff;
+  top: 40%;
+  left:50%;
+  transform: translate(-50%, -50%);
+}
+button{
+  padding: .5rem 2rem;
+  text-align: left;
+}
+</style>
--- src/Plinko/src/components/Sidebar.vue
+++ src/Plinko/src/components/Sidebar.vue
@@ -11,11 +11,11 @@
                 {{ item.label }}
             </button>
         </div> -->
-        
+
         <div class="flex items-center justify-between flex-row gap-2">
-         
+
             <button @click="" class="autoBtn rounded-full  mx-2">
-              
+
             </button>
             <div class="flex justify-end items-center">
                 <button
@@ -38,15 +38,15 @@
               <button class="halfBtn"></button>
               <button class="reduceBtn"></button>
               <div class="flex flex-col mt-[-10px]">
-                <label for="betAmount" class="text-xs text-[#45698C] font-medium">Bet Amount</label>
+                <label for="betAmount" class="text-xs text-[#45698C] font-medium">{{$t('BetAmount')}}</label>
                 <p class="bg-transparent border-0 text-[#00F320] text-xs font-bold">0.00001</p>
-                
+
               </div>
               <button class="addBtn"></button>
               <button class="towxBtn"></button>
               <button class="maxBtn"></button>
           </div>
-          
+
         </div>
         <div v-if="betMode === BetMode.AUTO">
             <div class="flex flex-col gap-1">
@@ -108,8 +108,8 @@
                   <p class=" text-[#45698C] text-xs py-1">Mouth Size</p>
               </div>
         </div>
-      
-    
+
+
 
         <button
             v-if="simulation.isSimulationing"
@@ -330,19 +330,19 @@ select option {
     background: url(../assets/images/redBtn.png) no-repeat;
     background-size: cover;
         &:active{
-          background: url(../assets/images/redBtn_pass.png) no-repeat;   
-          background-size: contain;  
+          background: url(../assets/images/redBtn_pass.png) no-repeat;
+          background-size: contain;
         }
   }
   &.colorBall{
     background: url(../assets/images/colorBtn.png) no-repeat;
     background-size: cover;
     &:active{
-          background: url(../assets/images/colorBtn_pass.png) no-repeat; 
-          background-size: contain;    
+          background: url(../assets/images/colorBtn_pass.png) no-repeat;
+          background-size: contain;
         }
   }
-  
+
 }
 .betContent{
       .minBtn{

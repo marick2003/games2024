eee53e016bae9b283dc26f5ed6e038b0c24b0f11
rocky <rocky@evervictory-it.com>
2024-12-12 09:34:45 +0800

    remove redbtn pass

--- src/Plinko/src/stores/app.ts
+++ src/Plinko/src/stores/app.ts
@@ -65,7 +65,7 @@ export const useAppStore = defineStore('App', () => {
         pin:  new URL('@/assets/images/pin.png', import.meta.url).href,
         red:  new URL('@/assets/images/red.png', import.meta.url).href,
         redBtn:  new URL('@/assets/images/redBtn.png', import.meta.url).href,
-        redBtn_pass:  new URL('@/assets/images/redBtn_pass.png', import.meta.url).href,
+        //redBtn_pass:  new URL('@/assets/images/redBtn_pass.png', import.meta.url).href,
         // muteSvg:  new URL('@/assets/images/mute.svg', import.meta.url).href,
         // soundSvg:  new URL('@/assets/images/sound.svg', import.meta.url).href,
         // settingSvg:  new URL('@/assets/images/setting.svg', import.meta.url).href,

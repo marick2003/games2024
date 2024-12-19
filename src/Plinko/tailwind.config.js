5599a3999cf8c0a08226524f5f59292b32c8bbd2
rick_ma202 <rick_ma202@fsoffice.com>
2024-12-19 17:52:27 +0800

    移除tailwind config

--- src/Plinko/tailwind.config.js
+++ src/Plinko/tailwind.config.js
@@ -4,16 +4,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        '.text-xxs': {
-          fontSize: '12px', /* 基準字體大小 */
-          transform: 'scale(0.8333)', /* 縮放到 10px */
-          transformOrigin: 'left', /* 縮放從左側開始 */
-          display: 'inline-block', /* 保證縮放效果正確 */
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
